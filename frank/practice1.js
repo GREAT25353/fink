46e9cb2b0a7bdad2f70f9e247d35bd87dd2ed875
GREAT25353 <66916710+GREAT25353@users.noreply.github.com>
2025-02-23 21:20:21 +0000

    free

---
+++ frank/practice1.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "jhonDopkins123@gmail.com"
+var accountPassword = "45667"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+accountEmail = "rauna34@gmail.com"
+accountPassword = "213343243"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
