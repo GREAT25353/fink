39b3fa26338af7fc4a61e202a2193bd39521af8a
GREAT25353 <66916710+GREAT25353@users.noreply.github.com>
2025-02-26 09:26:50 +0000

    sa

---
+++ frank/sweet.js
@@ -0,0 +1,26 @@
+//Comparing same data type
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// comparing different data type
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// is tarah ke conversion aapko confusion mein dal sakte hain
+// inhe avoid karna chahiye
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// strict check - ===
+console.log("2" === 2);
+
+
+
